be4a3497f13331479d1057c96a2f0d2810888043
trivonahospitality <trivonahospitality@gmail.com>
2026-07-08 22:25:14 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -143,17 +143,3 @@ window.addEventListener("scroll", () => {
     }
 
 });
-/* ===== Mobile Menu ===== */
-
-const menuToggle = document.getElementById("menu-toggle");
-const navbar = document.getElementById("navbar");
-
-if(menuToggle && navbar){
-
-menuToggle.addEventListener("click",function(){
-
-navbar.classList.toggle("active");
-
-});
-
-}
